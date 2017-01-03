f44ce2eb6e09d112b31ef7bea53ee2274875222c
KimStationær <krimmero@hotmail.dk>
2017-01-03 17:07:53 +0100

    search lastname created, testet and working

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+///Twig autoload - rightclick on project - composer x 2
 require_once 'vendor/autoload.php';
 Twig_Autoloader::register();
 
@@ -7,48 +9,61 @@ $twig = new Twig_Environment($loader, array(
     'auto_reload' => true
 ));
 
-
-
-
-//echo $result;
-
-
-
-
-
-
+///Puts out entire list///
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $url = 'http://localhost:25134/EmployeeService.svc/GetEmployeeList/';
 curl_setopt($curl, CURLOPT_URL, $url);
 $response = curl_exec($curl);
 
+///To check response///
 //echo $response;
-
 
 curl_close($curl);
 $jsonObj = json_decode($response);
 
+///print_r puts out object better looking///
 //echo print_r($jsonObj);
 
-$id = "1";
 
+$id = "";
+/// if idValue in form is input, set $id to idValue///
 if (isset($_POST['idValue']))
 {
     $id = $_POST['idValue'];
 }
 
+///Puts out item with the matching $id///
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $url = 'http://localhost:25134/EmployeeService.svc/GetEmployeeById/'.$id;
 curl_setopt($curl, CURLOPT_URL, $url);
 $result = curl_exec($curl);
 
-
 curl_close($curl);
 $jsonObjId = json_decode($result);
 
+$name = "";
+/// if searchData in form is input, set $name to searchData///
+if (isset($_POST['searchData']))
+{
+    $name = $_POST['searchData'];
+}
+
+///Puts out item with the matching fragtment of lastname///
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$url = 'http://localhost:25134/EmployeeService.svc/GetEmployeeByLastName/'.$name;
+curl_setopt($curl, CURLOPT_URL, $url);
+$nameResult = curl_exec($curl);
 
 
+
+curl_close($curl);
+$jsonObjName = json_decode($nameResult);
+
+//echo print_r($jsonObjName);
+
+///Twig template to put the wanted data to the html///
 $template = $twig->loadTemplate('index.html.twig');
-echo $template->render(array('title' => 'Employee PHPApp', 'json' => $jsonObj, 'jsonId' => $jsonObjId));
+echo $template->render(array('title' => 'Employee PHPApp', 'json' => $jsonObj, 'jsonId' => $jsonObjId, 'jsonName' => $jsonObjName));
